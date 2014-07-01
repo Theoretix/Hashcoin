5a8251f177c2f2b5211ddf2a2d5d13c4cc3a8a2b
Dr Roy Murphy <RoyMurphy@users.noreply.github.com>
2014-07-02 00:23:03 +0100

    Update Copyright

--- src/hashcoind.cpp
+++ src/hashcoind.cpp
@@ -1,6 +1,5 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2012 The Hashcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2009-2014 Theortix Consortium
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "init.h"
